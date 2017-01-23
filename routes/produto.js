74fc6d0d714c3aac8d76305b8f9cbef5d612db7c
Diogo Silverio <dblsilverio@gmail.com>
2017-01-23 21:41:43 -0200

    Adicionado nodemon ao projeto e alterado o script npm de start

--- routes/produto.js
+++ routes/produto.js
@@ -1,3 +1,5 @@
 
-module.exports = (app) => 
+module.exports = (app) => {
 	app.get("/produtos", (req, res) => res.render("produto/lista"));
+	app.get("/produtos/:id", (req, res) => res.render("produto/lista"));
+}
