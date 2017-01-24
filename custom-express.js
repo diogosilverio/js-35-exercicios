5ad0e42d0e60a8e8455d6fb46d3ebd98e98c7dfe
Diogo Silverio <dblsilverio@gmail.com>
2017-01-24 21:41:19 -0200

    Inclusao de body parser. Criacao de rota POST:/produtos e respectivo formulario.

--- custom-express.js
+++ custom-express.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require("body-parser");
+
 // module.exports = function(){
 	app.set("view engine", "ejs");
 
 	app.use(express.static("public"));
+	app.use(bodyParser.urlencoded({extended: true}));
 
 	require("./routes/produto.js")(app);
 	require("./routes/cliente.js")(app);
--- routes/produto.js
+++ routes/produto.js
@@ -2,7 +2,8 @@ const connectionFactory = require("../infra/connection-factory")
 const ProdutoDAO = require("../infra/ProdutoDAO");
 
 module.exports = (app) => {
-	app.get("/produtos", (req, res) => {
+	app.route("/produtos")
+	.get((req, res) => {
 
 		let connection = connectionFactory();
 		let dao = new ProdutoDAO(connection);
@@ -10,10 +11,17 @@ module.exports = (app) => {
 		dao.lista()
 			.then(
 				livros => res.render("produto/lista", {lista: livros}),
-				err => console.log(err)
+				err => {
+					console.log("Erro ocorreu: " + err.message);
+					res.sendStatus(500);
+				}
 			);
 
+	})
+	.post((req, res) => {
+		console.log(req.body);
+		res.sendStatus(201);
 	});
 
-	app.get("/produtos/:id", (req, res) => res.render("produto/lista"));
+	app.get("/produtos/form", (req, res) => res.render("produto/form"));
 }
