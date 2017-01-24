d4466fc6308d9d339240c85d44298cd1bdf9992f
Diogo Silverio <dblsilverio@gmail.com>
2017-01-24 19:25:18 -0200

    Consulta de livros com modulo mysql na rota /produtos

--- routes/produto.js
+++ routes/produto.js
@@ -1,5 +1,27 @@
 
+const mysql = require("mysql");
+
 module.exports = (app) => {
-	app.get("/produtos", (req, res) => res.render("produto/lista"));
+	app.get("/produtos", (req, res) => {
+
+		let connection = mysql.createConnection({
+			host: "localhost",
+			user: "root",
+			password: "",
+			database: "casadocodigo"
+		});
+
+		connection.query("select * from livros", (err, result) => { //Callback error first*
+			if(err){
+				return console.log(err);
+			}
+
+			res.render("produto/lista", {lista: result});
+			
+			
+		});
+
+	});
+
 	app.get("/produtos/:id", (req, res) => res.render("produto/lista"));
 }
